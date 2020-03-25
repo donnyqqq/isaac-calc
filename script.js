4964b979675af44bfd82bb6bb454c911929233fd
Daniil Fomin <donatello.fomin@gmail.com>
2020-03-25 16:40:40 +0200

    changes

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let damage = 3.50
-
+let tears = 10
 let polyph = {
     id: 169,
     dmg: (damage+4)*2,
-
+    hrt: 0,
+    trs: (tears*2.1)+3,
+    rng: 0,
+    spdShot: 0,
+    spd: 0,
+    luck: 0
 }
-
-alert(polyph.dmg)
+let brim = {
+    id: 118,
+    dmg: damage,
+    hrt: 0,
+    trs: tears-(tears*3),
+    rng: 0,
+    spdShot: 0,
+    spd: 0,
+    luck: 0
+}
