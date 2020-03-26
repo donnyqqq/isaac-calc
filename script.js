c6f55f9f02834bfde40452ee4cd8debd6641b37f
Daniil Fomin <donatello.fomin@gmail.com>
2020-03-26 11:19:54 +0200

    changes

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ let brim = {
     spd: 0,
     luck: 0
 }
+let art1;
+let art2;
+let art1id;
+let art2id;
+function setStats(item1, item2){
+let slot = document.getElementById('stats')
+let damag = Number(item1.dmg) + Number(item2.dmg)
+let heartz = Number(item1.hrt) + Number(item2.hrt)
+let tearz = Number(item1.trs) + Number(item2.trs)
+let rang = Number(item1.rng) + Number(item2.rng)
+let shsp = Number(item1.spdShot) + Number(item2.spdShot)
+let sped = Number(item1.spd) + Number(item2.spd)
+let luzck = Number(item1.luck) + Number(item2.luck)
+slot.innerHTML = '<strong>Damage:</strong>' + damag + '</br> <strong>Hearts:</strong> ' + heartz + '</br> <strong>Tears(delay):</strong>' + tearz + '</br> <strong>Range:</strong>' + rang + '</br> <strong>Shot Speed:</strong>' + shsp + '</br> <strong>Speed:</strong>' + sped + '</br> <strong>Luck:</strong>' + luzck
+}
+function setArt(item){
+    if(!art1){
+        art1id = item.id
+        art1 = item
+    }else if(!art2){
+        if(item != art1){
+    art2id = item.id
+    art2 = item}
+     setStats(art1, art2)
+    }}
